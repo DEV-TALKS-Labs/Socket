3e6dbd43253f25f0be2fbcf6e8a434abbce19dcc
sarahassan <sarraree5@gmail.com>
2023-12-06 17:01:38 +0200

    [ROOM] message and user join

--- src/controllers/roomControllers/joinRoom.js
+++ src/controllers/roomControllers/joinRoom.js
@@ -4,33 +4,9 @@ import dotevn from "dotenv";
 dotevn.config();
 const API_URL = process.env.API_URL;
 
-const joinRoom = async (io, socket, { handle, roomId }) => {
-  console.log("a user connected: ", socket.id);
+const joinRoom = async (io, socket, roomId) => {
   socket.join(roomId);
-  socket.broadcast.to(roomId).emit("user:joined", { handle });
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
-    // socket.to(roomId).broadcast.emit("user-leave", handle);
-  });
-  //   try {
-  //     const res = await axios.post(API_URL + "rooms", room);
-  //     if (res.status === 201) {
-  //       io.emit("add room", {
-  //         success: "room added successfully",
-  //         body: res.data,
-  //       });
-  //     } else {
-  //       socket.emit("errorHandler", {
-  //         error: "faild to add room",
-  //         message: res.data,
-  //       });
-  //     }
-  //   } catch (err) {
-  //     socket.emit("errorHandler", {
-  //       error: "faild to add task",
-  //       message: err.message,
-  //     });
-  //   }
+  socket.broadcast.to(roomId).emit("user:joined", { handle: "handle" });
 };
 
 export default joinRoom;
--- src/controllers/socketController.js
+++ src/controllers/socketController.js
@@ -5,6 +5,7 @@ const configureSocket = (io, socket) => {
   socket.on("test", (data) => io.emit("test:res", data));
   socket.on("room:add", (room) => addRoom(io, socket, room));
   socket.on("user:joinRoom", (room) => joinRoom(io, socket, room));
+  socket.on("message:send", ({message, roomId}) => io.to(roomId).emit("message:receive", message));
 };
 
 export default configureSocket;
