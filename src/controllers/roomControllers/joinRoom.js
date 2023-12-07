21a1ee4952709f30eb865edff2e96f722eb2dc3c
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2023-12-07 18:18:59 +0200

    [USER] join and leave room

--- src/controllers/roomControllers/joinRoom.js
+++ src/controllers/roomControllers/joinRoom.js
@@ -1,12 +1,6 @@
-import axios from "axios";
-import dotevn from "dotenv";
-
-dotevn.config();
-const API_URL = process.env.API_URL;
-
-const joinRoom = async (io, socket, roomId) => {
+const joinRoom = async (io, socket, {roomId, user}) => {
   socket.join(roomId);
-  socket.broadcast.to(roomId).emit("user:joined", { handle: "handle" });
+  io.to(roomId).emit("user:joined", user);
 };
 
 export default joinRoom;
--- src/controllers/roomControllers/leaveRoom.js
+++ src/controllers/roomControllers/leaveRoom.js
@@ -6,18 +6,19 @@ const API_URL = process.env.API_URL;
 
 const leaveRoom = async (io, socket, { roomId, userId }) => {
   try {
-    const res = await axios.patch(`${API_URL}rooms/${roomId}/leave`, 
-    {
+    const res = await axios.patch(`${API_URL}rooms/${roomId}/leave`, {
       id: userId,
     });
+
     socket.leave(roomId);
+
     if (res.status === 204) {
       io.emit("room:delete", roomId);
-      io.to(roomId).broadcast.emit("user:left", userId);
+      io.to(roomId).emit("user:left", userId);
     } else if (res.status === 200) {
-      io.to(roomId).broadcast.emit("user:left", userId);
+      io.to(roomId).emit("user:left", userId);
     }
-} catch (err) {
+  } catch (err) {
     socket.emit("errorHandler", {
       error: "faild to leave room",
       message: err.message,
