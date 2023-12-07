0fa2ed65cd24d9ee27a41d98ba5732620682feaf
sarahassan <sarraree5@gmail.com>
2023-12-07 09:55:05 +0200

    emit an envent to redirect the host to the room when created

--- src/controllers/roomControllers/addRoom.js
+++ src/controllers/roomControllers/addRoom.js
@@ -7,14 +7,13 @@ const API_URL = process.env.API_URL;
 const addRoom = async (io, socket, data) => {
   
   try {
-    console.log(API_URL + "rooms");
     const res = await axios.post(API_URL + "rooms", data.room, data.headers);
-    console.log("2");
     if (res.status === 201) {
       io.emit("room:new", {
         success: "room added successfully",
         body: res.data,
       });
+      socket.emit("user:redirect", res.data.id)
     } else {
       socket.emit("errorHandler", {
         error: "faild to add room",
@@ -22,7 +21,6 @@ const addRoom = async (io, socket, data) => {
       });
     }
   } catch (err) {
-    console.log(err);
     socket.emit("errorHandler", {
       error: "faild to add task",
       message: err.message,
